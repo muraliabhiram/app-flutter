7c57a70375242ecb1786956d318151473e48e1e8
muraliabhiram <muraliabhiram@gmail.com>
2020-06-13 00:22:51 +0530

    added app bar

--- helloFlutter/lib/main.dart
+++ helloFlutter/lib/main.dart
@@ -74,11 +74,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text('I am intreseted'),
+            Text('I am intreseted  Murali '),
             RichText(
               text:TextSpan(
                 style: DefaultTextStyle.of(context).style,
